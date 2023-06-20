88d44a884f53498db6b549eff00f4d6fa8afe6c5
Egor Cherniak <cherniak3@yandex.ru>
2023-06-20 09:19:38 +0300

    Add missing fields

--- src/main/java/dev/vality/alerting/mayday/client/model/prometheus/PrometheusRuleSpec.java
+++ src/main/java/dev/vality/alerting/mayday/client/model/prometheus/PrometheusRuleSpec.java
@@ -17,16 +17,23 @@ public class PrometheusRuleSpec {
     @NoArgsConstructor
     public static class Group {
         private String name;
+        private String duration;
         private Set<Rule> rules;
+        @JsonProperty("partial_response_strategy")
+        private String partialResponseStrategy;
+        private Integer limit;
     }
 
     @Data
     @NoArgsConstructor
     public static class Rule {
+        private String record;
         private String alert;
         private String expr;
         @JsonProperty("for")
         private String duration;
+        @JsonProperty("keep_firing_for")
+        private String keepFiringFor;
         private Map<String, String> labels;
         private Map<String, String> annotations;
     }
