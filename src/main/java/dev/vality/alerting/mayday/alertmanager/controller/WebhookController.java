69a407b1290b046df5d98faaf4a8bdecb85b4d75
Egor Cherniak <cherniak3@yandex.ru>
2024-01-26 20:53:30 +0300

    Extend webhook logging

--- src/main/java/dev/vality/alerting/mayday/alertmanager/controller/WebhookController.java
+++ src/main/java/dev/vality/alerting/mayday/alertmanager/controller/WebhookController.java
@@ -1,5 +1,6 @@
 package dev.vality.alerting.mayday.alertmanager.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.vality.alerting.mayday.alertmanager.config.properties.AlertmanagerWebhookProperties;
 import dev.vality.alerting.mayday.alertmanager.constant.WebhookStatus;
 import dev.vality.alerting.mayday.alertmanager.model.Webhook;
@@ -10,14 +11,17 @@ import dev.vality.alerting.tg_bot.NotifierServiceSrv;
 import dev.vality.alerting.tg_bot.ReceiverNotFound;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.apache.thrift.TException;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -29,28 +33,36 @@ public class WebhookController {
     private final NotifierServiceSrv.Iface telegramBotClient;
     private final Converter<Webhook.Alert, Notification> webhookAlertToNotificationConverter;
     private final AlertmanagerService alertmanagerService;
+    private final ObjectMapper objectMapper;
 
     @PostMapping(value = "/webhook", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> processWebhook(@RequestBody Webhook webhook) {
-        log.info("Received webhook from alertmanager: {}", webhook);
-        for (Webhook.Alert alert : webhook.getAlerts()) {
-            try {
-                String userId = alert.getLabels().get(PrometheusRuleLabel.USERNAME);
-                String alertName = alert.getLabels().get(PrometheusRuleLabel.ALERT_NAME);
-                // Алертменеджер может прислать нотификацию уже после того, как пользователь удалил алерт, т.к
-                // обновления в конфигурации применяются не моментально. Поэтому нужна доп.фильтрация здесь.
-                if (isResolvedNotificationPermitted(webhook)
-                        && alertmanagerService.containsUserRoute(userId, alertName)) {
-                    var notification = webhookAlertToNotificationConverter.convert(alert);
-                    telegramBotClient.notify(notification);
-                    log.info("Alertmanager webhook processed successfully: {}", webhook);
+    public ResponseEntity<String> processWebhook(HttpServletRequest servletRequest) {
+        try {
+            var webhookBody = servletRequest.getReader().lines().collect(Collectors.joining(" "));
+            log.info("Received webhook from alertmanager: {}", webhookBody);
+            var webhook = objectMapper.readValue(webhookBody, Webhook.class);
+            for (Webhook.Alert alert : webhook.getAlerts()) {
+                try {
+                    String userId = alert.getLabels().get(PrometheusRuleLabel.USERNAME);
+                    String alertName = alert.getLabels().get(PrometheusRuleLabel.ALERT_NAME);
+                    // Алертменеджер может прислать нотификацию уже после того, как пользователь удалил алерт, т.к
+                    // обновления в конфигурации применяются не моментально. Поэтому нужна доп.фильтрация здесь.
+                    if (isResolvedNotificationPermitted(webhook)
+                            && alertmanagerService.containsUserRoute(userId, alertName)) {
+                        var notification = webhookAlertToNotificationConverter.convert(alert);
+                        telegramBotClient.notify(notification);
+                        log.info("Alert from alertmanager webhook processed successfully: {}", alert);
+                    }
+                } catch (ReceiverNotFound receiverNotFound) {
+                    log.error("Unable to find notification receiver '{}':", webhook.getReceiver(), receiverNotFound);
+                } catch (TException e) {
+                    log.error("Unexpected error during notification delivery:", e);
+                    return ResponseEntity.internalServerError().build();
                 }
-            } catch (ReceiverNotFound receiverNotFound) {
-                log.error("Unable to find notification receiver '{}':", webhook.getReceiver(), receiverNotFound);
-            } catch (TException e) {
-                log.error("Unexpected error during notification delivery:", e);
-                return ResponseEntity.internalServerError().build();
             }
+        } catch (Exception e) {
+            log.error("Unexpected error during webhook parsing:", e);
+            return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.ok().build();
     }
